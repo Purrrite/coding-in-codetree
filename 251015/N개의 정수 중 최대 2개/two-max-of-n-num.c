62f20d0ad4b956f4035765b085abb34a63b32fd6
Purrrite <deliciousjelly2@gmail.com>
2025-10-15 21:34:25 +0900

    Title: N개의 정수 중 최대 2개, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251015/N개의 정수 중 최대 2개/two-max-of-n-num.c
+++ 251015/N개의 정수 중 최대 2개/two-max-of-n-num.c
@@ -11,13 +11,17 @@ int main() {
 
     int max_num = -2147483648;
     int second_max_num = -2147483648;
+    int max_num_index;
 
     for (int i = 0; i < N; i++) {
-        if (max_num < arr[i]) max_num = arr[i]; // 2147483647 
+        if (max_num < arr[i]) {
+            max_num = arr[i];
+            max_num_index = i;
+        }
     }
 
     for (int i = 0; i < N; i++) {
-        if (second_max_num < arr[i] && arr[i] < max_num) second_max_num = arr[i]; // 100
+        if (second_max_num < arr[i] && i != max_num_index) second_max_num = arr[i]; // 100
     }
 
     printf("%d %d\n", max_num, second_max_num);
