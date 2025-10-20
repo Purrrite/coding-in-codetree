66bb390a4dbb06269cbcd19ebc9b5c704b8382af
Purrrite <deliciousjelly2@gmail.com>
2025-10-20 19:04:36 +0900

    Title: 배열의 합, Time: 9ms, Memory: 0MB, Status: Passed - Codetree

--- 251020/배열의 합/sum-of-array.c
+++ 251020/배열의 합/sum-of-array.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
+int arr[4][4];
+int sum_arr[4];
+
 int main(void) {
     // Please write your code here.
-    int arr[4][4];
-int sum_arr[4];
     for (int i = 0; i < 4; i++) {
         for (int j = 0; j < 4; j++) {
-            scanf("%d", arr[i][j]);
+            scanf("%d", &arr[i][j]);
         }
     }
 
