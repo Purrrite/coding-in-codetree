9d650b981347057850de97107a9de34e5dc9fcbb
Purrrite <deliciousjelly2@gmail.com>
2025-10-25 17:10:05 +0900

    Title: 격자에 점 그리기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251025/격자에 점 그리기/draw-points-on-grid.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    // Please write your code here.
+    int arr_size, coin_number;
+    cin >> arr_size >> coin_number;
+
+    vector<vector<int>> coin_arr(arr_size, vector<int>(arr_size, 0));
+
+    int count = 1;
+
+    for (int i = 0; i < coin_number; i++)
+    {
+        int input_r, input_c;
+        cin >> input_r >> input_c;
+        int r = input_r - 1;
+        int c = input_c - 1;
+
+        coin_arr[r][c] = count;
+        count++;
+    }
+
+    for (int col = 0; col < arr_size; col++)
+    {
+        for (int row = 0; row < arr_size; row++)
+        {
+            cout << coin_arr[col][row] << ' ';
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
