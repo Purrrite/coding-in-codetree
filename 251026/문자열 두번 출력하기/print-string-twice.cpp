55c765b4fe07447b9879eb6043cb65cec73b01c1
Purrrite <deliciousjelly2@gmail.com>
2025-10-26 20:00:23 +0900

    Title: 문자열 두번 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251026/문자열 두번 출력하기/print-string-twice.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    string txt;
+    cin >> txt;
+
+    cout << txt << endl;
+    cout << txt << endl;
+    return 0;
+}
