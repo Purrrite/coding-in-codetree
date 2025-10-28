a6f223b54c6e456f74a6615b66b2daaacd2a01d5
Purrrite <deliciousjelly2@gmail.com>
2025-10-28 12:13:21 +0900

    Title: 가장 짧은 문자열, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251028/가장 짧은 문자열/shortest-string.c
+++ 251028/가장 짧은 문자열/shortest-string.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 #include <string.h>
 
+#define SIZE 3
+#define MAX_CHAR 20
+
 int main() {
     // Please write your code here.
-    char inputs[3][20];
-    int input_length[3];
+    char inputs[SIZE][MAX_CHAR];
+    int input_length[SIZE];
 
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < SIZE; i++) {
         scanf("%s", &inputs[i]);
         input_length[i] = strlen(inputs[i]);
     }
 
     int len_max = 0;
-    int len_min = 20;
-    for (int i = 0; i < 3; i++) {
+    int len_min = MAX_CHAR;
+    for (int i = 0; i < SIZE; i++) {
         len_max = input_length[i] > len_max ? input_length[i] : len_max;
         len_min = input_length[i] < len_min ? input_length[i] : len_min;
     }
