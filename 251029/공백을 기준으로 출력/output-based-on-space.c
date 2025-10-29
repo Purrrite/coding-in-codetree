cf96574a42b00c89bfcaa24ce3a0adf19f9416e6
Purrrite <deliciousjelly2@gmail.com>
2025-10-29 15:58:22 +0900

    Title: 공백을 기준으로 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251029/공백을 기준으로 출력/output-based-on-space.c
+++ 251029/공백을 기준으로 출력/output-based-on-space.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-char string1[101], string2[101], output[202];
+char string1[101], string2[101];
 
 int main()
 {
@@ -12,11 +12,11 @@ int main()
     int string2_length = strlen(string2);
 
     for (int i = 0; i < string1_length; i++)
-        if (string1[i] != (char)' ' && string1[i] != '\n')
+        if (string1[i] != ' ' && string1[i] != '\n')
             printf("%c", string1[i]);
 
     for (int i = 0; i < string2_length; i++)
-        if (string2[i] != (char)' ' && string1[i] != '\n')
+        if (string2[i] != ' ' && string2[i] != '\n')
             printf("%c", string2[i]);
 
     return 0;
