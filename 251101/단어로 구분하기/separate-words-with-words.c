0a308336410ed965906ee494627c9fadbcc3e848
Purrrite <deliciousjelly2@gmail.com>
2025-11-01 18:24:09 +0900

    Title: 단어로 구분하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251101/단어로 구분하기/separate-words-with-words.c
+++ 251101/단어로 구분하기/separate-words-with-words.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 
-char string[200];
+char string[201];
 
 int main()
 {
-    fgets(string, 200, stdin);
+    fgets(string, 201, stdin);
     int string_length = strlen(string);
 
     for (int i = 0; i < string_length; i++) {
