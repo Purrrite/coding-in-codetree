24162f88f2ee06a4c5e865585577e16f3da71faa
Purrrite <deliciousjelly2@gmail.com>
2025-11-05 14:42:23 +0900

    Title: 문자열 추가하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251105/문자열 추가하기/add-spring.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string letter;
+
+int main() {
+    // Please write your code here.
+    cin >> letter;
+    string output = letter + "Hello";
+    cout << output << endl;
+    return 0;
+}
