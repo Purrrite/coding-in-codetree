484f89f013e10df7c64c3bef8a47ade1e17abd77
Purrrite <deliciousjelly2@gmail.com>
2025-11-11 13:39:45 +0900

    Title: 문자 교체하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251111/문자 교체하기/changing-char.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    string str1, str2;
+    cin >> str1 >> str2;
+
+    for (int i = 0; i < 2; i++) {
+        str2[i] = str1[i];
+    }
+
+    cout << str2;
+
+    return 0;
+}
