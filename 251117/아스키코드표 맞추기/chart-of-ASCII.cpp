4ca23b4a244ff81442ccfe6e67123662f5a11512
Purrrite <deliciousjelly2@gmail.com>
2025-11-17 17:18:50 +0900

    Title: 아스키코드표 맞추기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251117/아스키코드표 맞추기/chart-of-ASCII.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int N;
+    
+    for (int i = 0; i < 5; i++) {
+        cin >> N;
+        cout << (char)N << ' ';
+    }
+
+    cout << endl;
+    return 0;
+}
