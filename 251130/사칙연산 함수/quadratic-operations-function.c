7b9e4fab9958bfab3c55c750a362ab5fbd13856e
Purrrite <deliciousjelly2@gmail.com>
2025-11-30 17:24:15 +0900

    Title: 사칙연산 함수, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251130/사칙연산 함수/quadratic-operations-function.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+int add(int *a, int *c) {
+    printf("%d + %d = %d",*a, *c, *a + *c);
+}
+
+int subtract(int *a, int *c) {
+    printf("%d - %d = %d",*a, *c, *a - *c);
+}
+
+int multiply(int *a, int *c) {
+    printf("%d * %d = %d",*a, *c, *a * *c);
+}
+
+int divide(int *a, int *c) {
+    printf("%d * %d = %d",*a, *c, *a / *c);
+}
+
+int main()
+{
+    int a, c;
+    char o;
+
+    scanf("%d %c %d", &a, &o, &c);
+
+    switch (o)
+    {
+    case '+':
+    {
+        add(&a, &c);
+        break;
+    }
+    case '-':
+    {
+        subtract(&a, &c);
+        break;
+    }
+    case '*':
+    {
+        multiply(&a, &c);
+        break;
+    }
+    case '/':
+    {
+        divide(&a, &c);
+        break;
+    }
+    default:
+        printf("False");
+        break;
+    }
+
+    return 0;
+}
