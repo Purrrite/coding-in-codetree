46adae073e54adc1c61c4c0b3962e4fd2b9f66af
Purrrite <deliciousjelly2@gmail.com>
2025-12-01 13:24:39 +0900

    Title: 짝수만 2로 나누기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251201/짝수만 2로 나누기/divide-even-numbers-by-2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int n;
+int arr[50];
+
+void divide(int *input_arr) {
+    for (int i = 0; i < n; i++) {
+        if (*(input_arr + i) % 2 == 0) {
+            *(input_arr + i) /= 2;
+        }
+    }
+}
+
+int main() {
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", arr + i);
+    }
+    // Please write your code here.
+    divide(arr);
+
+    for (int i = 0; i < n; i++) {
+        printf("%d ", *(arr + i));
+    }
+    printf("\n");
+    
+    return 0;
+}
