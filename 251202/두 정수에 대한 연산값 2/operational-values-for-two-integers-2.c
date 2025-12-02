e0a57c2ccc75b26e0609d117eff03575114d299e
Purrrite <deliciousjelly2@gmail.com>
2025-12-02 17:33:13 +0900

    Title: 두 정수에 대한 연산값 2, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 251202/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.c
+++ 251202/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
 void compute(int *a, int *b) { //a와 b는 같지 않음.
-    *a = *a > *b ? *a * 2 : *a + 10;
-    *b = *b > *a ? *b * 2 : *b + 10;
+    int x = *a, y = *b;
+
+    *a = x > y ? x * 2 : x + 10;
+    *b = y > x ? y * 2 : y + 10;
 }
 
 int main() {
